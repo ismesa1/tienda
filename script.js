7f60aaee2731c8c9336f225fba5afeefd70ab48b
ismesa1 <149906242+ismesa1@users.noreply.github.com>
2025-05-05 15:21:53 -0500

    feat:Creación clase, con div, label e input en la tarjeta

---
+++ script.js
@@ -0,0 +1,10 @@
+function init(){
+    var productos= document.getElementById("producto")
+    productos = productos.innerHTML = 
+    `
+    <div>
+        <label> Producto A </label>
+        <input min ="1" type="number" name="" id="">
+        <button  class="btn btn-primary" type="button">Agregar</button>
+    </div>`
+}
